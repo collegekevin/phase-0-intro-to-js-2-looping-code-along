aca2cfe41e753c77151eacd6fea1670a29ffd414
Kevin Price <collegekevin@gmail.com>
2022-09-24 10:02:13 -0700

    commit tree 782b7621f2f6e6e24f047c660448ed4978d25353

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll", "ninendo switch", "ipad", "backpack"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(giftGiverNames, eventName){
+    const listOfCards = [];
+    for (let i = 0; i <giftGiverNames.length; i++){
+        listOfCards.push(`Thank you, ${giftGiverNames[i]}, for the wonderful ${eventName} gift!`); 
+    }
+    return listOfCards;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+    return;
+}
